cc6d4896846fcd139126282d96f187d62badaad3
Moeez Muhammad <mmoeez48@gmail.com>
2019-01-09 23:35:22 -0500

    Inital commit
    This library is to allow easy rumbling of Xbox Controllers. The easiest
    use case is that of notifying drivers of different events such as
    value changes, warnings, etc. This library automatically rumbles with
    the set intensity on the set side, then turns it off quickly to simulate
    the rumbling in regular video games. Also, it can be run asynchronously
    to not impede with the rest of the regular robot operations.

---
+++ src/main/java/frc/robot/misc/Rumble.java
@@ -0,0 +1,52 @@
+package frc.robot.misc;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+
+import java.lang.Thread;
+
+public class Rumble implements Runnable{
+	/*
+		* This class (and it's only function) rumbles the controller anyway you like it.
+		* Use cases include notifying the driver of certain values
+		* It sets the rumble to the intensity you want, and then waits a certain time before turning it off
+		* To run this function in another part of the project, and have it done asynchronously: do it like this:
+			ExecutorService executor = Executors.newSingleThreadExecutor(); // only needed once
+			Rumble myRumble = new Rumble(values); // set preset Rumbles that you will be using beforehand with their values ; only needed once
+			executor.execute(myRumble); // whenever you need to rumble the controller, run this, passing in the Rumble object you created before
+
+	*/
+	private static final int sleepTime = 300;
+	private XboxController controller;
+	private String side;
+	private float intensity;
+
+	public void Rumble (XboxController controller, String side, float intensity) {
+		this.controller = controller;
+		this.side = side;
+		this.intensity = intensity;
+	}
+
+
+	@Override
+	public void run() {
+		try { // run() can't throw errors and Thread.sleep() can throw InterruptedException
+			if (side == "right")  {
+				controller.setRumble(RumbleType.kRightRumble, intensity);
+				Thread.sleep(sleepTime);
+				controller.setRumble(RumbleType.kRightRumble, 0.0);
+			} else if (side == "left") {
+				controller.setRumble(RumbleType.kLeftRumble, intensity);
+				Thread.sleep(sleepTime);
+				controller.setRumble(RumbleType.kLeftRumble, 0.0);
+			} else if (side == "both") {
+				controller.setRumble(RumbleType.kRightRumble, intensity);
+				controller.setRumble(RumbleType.kLeftRumble, intensity);
+				Thread.sleep(sleepTime);
+				controller.setRumble(RumbleType.kRightRumble, 0.0);
+				controller.setRumble(RumbleType.kLeftRumble, 0.0);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Rumble's sleep method returned an InterruptedException. Hope that never happens.");
+		}
+	}
+}
