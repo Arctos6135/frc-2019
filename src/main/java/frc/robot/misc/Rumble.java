794d3a7a41a61f69e9914d50d825aa52933c71db
Tyler Tian <tylertian123@gmail.com>
2019-01-27 15:29:52 -0500

    Add new parameters to Rumble and improve docs
    Rumble can now take two additional arguments; one specifies the duration
    in milliseconds to rumble for, and another one that allows the rumble to
    repeat for a number of times.

--- src/main/java/frc/robot/misc/Rumble.java
+++ src/main/java/frc/robot/misc/Rumble.java
@@ -7,54 +7,99 @@ import java.util.concurrent.Executors;
 import java.lang.Thread;
 
 /**
-	 * This class (and it's only function) rumbles the controller anyway you like it.
-	 * Use cases include notifying the driver of certain values.
-	 * It sets the rumble to the intensity you want, and then waits a certain time before turning it off
-	 * To run this function in another part of the project, and have it done asynchronously: do it like this
-	 * 
-		 * Rumble myRumble = new Rumble(values); // set preset Rumbles that you will be using beforehand with their values ; only needed once
-		 * myRumble.execute(); // whenever you need to rumble the controller, run this, using the Rumble object you created before
-	 */
+ * This class (and its only function) rumbles the controller anyway you like it.
+ * Use cases include notifying the driver of certain values.
+ * It sets the rumble to the intensity you want, and then waits a certain time before turning it off
+ * To run this function in another part of the project, and have it done asynchronously: do it like this
+ * <p>
+ * {@code
+ * Rumble myRumble = new Rumble(values); // set preset Rumbles that you will be using beforehand with their values ; only needed once
+ * myRumble.execute(); // whenever you need to rumble the controller, run this, using the Rumble object you created before
+ * }
+ * </p>
+ */
 public class Rumble implements Runnable {
 
-	private static final int sleepTime = 300;
-	private XboxController controller;
-	private String side;
-	private double intensity;
+    private final int sleepTime;
+    private final int repeatCount;
+	private final XboxController controller;
+	private final String side;
+	private final double intensity;
 	
-	private static ExecutorService executor = Executors.newCachedThreadPool();
+    private static ExecutorService executor = Executors.newCachedThreadPool();
+    
+    public static final String SIDE_LEFT = "left";
+    public static final String SIDE_RIGHT = "right";
+    public static final String SIDE_BOTH = "both";
 
 	/**
-	 @param controller The XboxController object that you're rumbling on
-	 @param side A string value that details the side to rumble: "left", "right", or "both"
-	 @param intensity The intensity to rumble at between 0 and 1
+     * Creates a new Rumble. It will last 300ms and will not repeat.
+	 * @param controller The XboxController object that you're rumbling on
+	 * @param side A string value that details the side to rumble: "left", "right", or "both"
+	 * @param intensity The intensity to rumble at between 0 and 1
 	 */
 	public Rumble (XboxController controller, String side, double intensity) {
 		this.controller = controller;
 		this.side = side;
-		this.intensity = intensity;
-	}
+        this.intensity = intensity;
+        
+        repeatCount = 1;
+        sleepTime = 300;
+    }
+    /**
+     * Creates a new Rumble. It will not repeat.
+     * @param controller The XboxController object that you're rumbling on
+     * @param side A string value that details the side to rumble: "left", "right", or "both"
+     * @param intensity The intensity to rumble at between 0 and 1
+     * @param duration The duration, in milliseconds, to rumble for
+     */
+    public Rumble (XboxController controller, String side, double intensity, int duration) {
+        this.controller = controller;
+        this.side = side;
+        this.intensity = intensity;
+        this.sleepTime = duration;
 
+        repeatCount = 1;
+    }
+    /**
+     * Creates a new Rumble.
+     * @param controller The XboxController object that you're rumbling on
+     * @param side A string value that details the side to rumble: "left", "right", or "both"
+     * @param intensity The intensity to rumble at between 0 and 1
+     * @param duration The duration, in milliseconds, to rumble for
+     * @param repeatCount The number of times to repeat this rumble
+     */
+    public Rumble (XboxController controller, String side, double intensity, int duration, int repeatCount) {
+        this.controller = controller;
+        this.side = side;
+        this.intensity = intensity;
+        this.sleepTime = duration;
+        this.repeatCount = repeatCount;
+    }
 
 	@Override
 	public void run() {
 		synchronized(this.controller) {
-			try { // run() can't throw errors and Thread.sleep() can throw InterruptedException
-				if (side == "right")  {
-					controller.setRumble(RumbleType.kRightRumble, intensity);
-					Thread.sleep(sleepTime);
-					controller.setRumble(RumbleType.kRightRumble, 0.0);
-				} else if (side == "left") {
-					controller.setRumble(RumbleType.kLeftRumble, intensity);
-					Thread.sleep(sleepTime);
-					controller.setRumble(RumbleType.kLeftRumble, 0.0);
-				} else if (side == "both") {
-					controller.setRumble(RumbleType.kRightRumble, intensity);
-					controller.setRumble(RumbleType.kLeftRumble, intensity);
-					Thread.sleep(sleepTime);
-					controller.setRumble(RumbleType.kRightRumble, 0.0);
-					controller.setRumble(RumbleType.kLeftRumble, 0.0);
-				}
+            try { // run() can't throw errors and Thread.sleep() can throw InterruptedException
+                for(int i = 0; i < repeatCount; i ++) {
+                    if (side.equals("right"))  {
+                        controller.setRumble(RumbleType.kRightRumble, intensity);
+                        Thread.sleep(sleepTime);
+                        controller.setRumble(RumbleType.kRightRumble, 0.0);
+                    } else if (side.equals("left")) {
+                        controller.setRumble(RumbleType.kLeftRumble, intensity);
+                        Thread.sleep(sleepTime);
+                        controller.setRumble(RumbleType.kLeftRumble, 0.0);
+                    } else if (side.equals("both")) {
+                        controller.setRumble(RumbleType.kRightRumble, intensity);
+                        controller.setRumble(RumbleType.kLeftRumble, intensity);
+                        Thread.sleep(sleepTime);
+                        controller.setRumble(RumbleType.kRightRumble, 0.0);
+                        controller.setRumble(RumbleType.kLeftRumble, 0.0);
+                    }
+                    
+                    Thread.sleep(sleepTime);
+                }
 			} catch (InterruptedException e) {
 				System.out.println("Rumble's sleep method returned an InterruptedException. Hope that never happens.");
 			}
