adc5726a6f5fa987b01504a61ebaba303f597cf9
Tyler Tian <tylertian123@gmail.com>
2019-01-30 22:01:13 -0500

    Change RotateToAngle to use isCompleted to determine completion
    RotateToAngle was using FollowTrajectory.isFinished in its isFinished
    method. This was changed to isCompleted.

--- src/main/java/frc/robot/commands/RotateToAngle.java
+++ src/main/java/frc/robot/commands/RotateToAngle.java
@@ -59,7 +59,7 @@ public class RotateToAngle extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return followerCommand.isFinished();
+        return followerCommand.isCompleted();
     }
 
     // Called once after isFinished returns true
