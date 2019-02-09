097042412de6fcb129f5db4642063ed780af4f18
Tyler Tian <tylertian123@gmail.com>
2019-02-09 13:57:10 -0500

    Fix various bugs and tune PIDs

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -71,7 +71,7 @@ public class TeleopDrive extends Command {
     protected void execute() {
         // Handle regular driving
         double x = OI.driverController.getRawAxis(OI.Controls.DRIVE_LEFT_RIGHT);
-        double y = OI.driverController.getRawAxis(OI.Controls.DRIVE_FWD_REV);
+        double y = -OI.driverController.getRawAxis(OI.Controls.DRIVE_FWD_REV);
         
         x = Math.abs(x) > DEADZONE ? x : 0;
         y = Math.abs(y) > DEADZONE ? y : 0;
@@ -81,7 +81,6 @@ public class TeleopDrive extends Command {
 
         if(reverseDrive) {
             y = -y;
-            x = -x;
         }
 
         double l = y + x, r = y - x;
