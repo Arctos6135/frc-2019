ad0b1efb2f9de6226a64f00dba2676433b19cff8
Tyler Tian <tylertian123@gmail.com>
2019-02-17 18:46:04 -0500

    Set RestartVisionServer to run in disabled as well

--- src/main/java/frc/robot/commands/RestartVisionServer.java
+++ src/main/java/frc/robot/commands/RestartVisionServer.java
@@ -15,6 +15,8 @@ public class RestartVisionServer extends InstantCommand {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         requires(Robot.vision);
+
+        setRunWhenDisabled(true);
     }
 
     // Called just before this Command runs the first time
