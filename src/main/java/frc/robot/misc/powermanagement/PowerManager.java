4fe3c58a428c3ad434aa29b29847a1d494adf8e9
Tyler Tian <tylertian123@gmail.com>
2019-02-21 14:58:14 -0500

    Add logging to power management class

--- src/main/java/frc/robot/misc/powermanagement/PowerManager.java
+++ src/main/java/frc/robot/misc/powermanagement/PowerManager.java
@@ -3,6 +3,7 @@ package frc.robot.misc.powermanagement;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
+import frc.robot.misc.RobotLogger;
 
 @SuppressWarnings("resource")
 public final class PowerManager {
@@ -12,9 +13,11 @@ public final class PowerManager {
         VoltageMonitoringTrigger trig = new VoltageMonitoringTrigger(lowestVoltage);
         trig.whenActive(new InstantCommand(() -> {
             RobotMap.compressor.stop();
+            RobotLogger.logWarning("Compressor stopped due to voltage too low");
         }));
         trig.whenInactive(new InstantCommand(() -> {
             RobotMap.compressor.start();
+            RobotLogger.logInfo("Compressor restarted after voltage has recovered");
         }));
     }
     
@@ -24,9 +27,11 @@ public final class PowerManager {
         trig.whenActive(new InstantCommand(() -> {
             regularMultiplier = Robot.drivetrain.getSpeedMultiplier();
             Robot.drivetrain.setSpeedMultiplier(multiplierWhenActivated);
+            RobotLogger.logWarning("Drivetrain max power output reduced to prevent brownout");
         }));
         trig.whenInactive(new InstantCommand(() -> {
             Robot.drivetrain.setSpeedMultiplier(regularMultiplier);
+            RobotLogger.logInfo("Drivetrian power back to normal after voltage has recovered");
         }));
     }
 
