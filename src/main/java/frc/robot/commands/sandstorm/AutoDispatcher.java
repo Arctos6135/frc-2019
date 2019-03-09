b9e158888644d5c44bbc7c2057acf8bd87caeeb3
Tyler Tian <tylertian123@gmail.com>
2019-03-09 15:31:54 -0500

    Remove all existing autos

--- src/main/java/frc/robot/commands/sandstorm/AutoDispatcher.java
+++ src/main/java/frc/robot/commands/sandstorm/AutoDispatcher.java
@@ -2,8 +2,6 @@ package frc.robot.commands.sandstorm;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
-import frc.robot.commands.FollowTrajectory;
-import frc.robot.misc.AutoPaths;
 
 /**
  * Finds the correct autonomous command based on parameters.
@@ -47,38 +45,6 @@ public final class AutoDispatcher {
      * @throws AutoNotFoundException If no matching auto command is found
      */
     public static final Command getAuto(HabLevel level, Mode mode) throws AutoNotFoundException {
-        if(mode == Mode.NONE) {
-            // Return an empty InstantCommand for no auto.
-            return new InstantCommand();
-        }
-        // Debug auto
-        else if(mode == Mode.DEBUG) {
-            return new FollowTrajectory(AutoPaths.debug);
-        }
-        switch(level) {
-        case ONE:
-            switch(mode) {
-            case ALIGNED:
-                return new HatchAutoHabLevelOneFrontAligned();
-            case LEFT:
-                return new HatchAutoHabLevelOneSide(mode);
-            case RIGHT:
-                return new HatchAutoHabLevelOneSide(mode);
-            case VISION:
-                return new HatchAutoHabLevelOneFrontVision();
-            default:
-                throw new AutoNotFoundException("No auto found");
-            }
-        case TWO:
-            switch(mode) {
-            case VISION: 
-                return new HatchAutoHabLevelTwoFrontVision();
-            default: throw new AutoNotFoundException("No auto found");
-            }
-        case THREE:
-            throw new AutoNotFoundException("No autos for hab level 3");
-        default:
-            throw new AutoNotFoundException("No auto found");
-        }
+        return new InstantCommand();
     }
 }
